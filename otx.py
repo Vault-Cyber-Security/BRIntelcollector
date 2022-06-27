f7d4e25f8c5b19cf5db98456fcf143be7218ebd9
mth <matheusoliveiratux4me@gmail.com>
2022-06-26 21:24:20 -0300

    :smile: escrevendo saida da consulta teste 2

--- otx.py
+++ otx.py
@@ -10,17 +10,18 @@ from OTXv2 import OTXv2, IndicatorTypes
 import pandas 
 
 load_dotenv()
-otx = OTXv2(dotenv_values('OTXKey'))
-#otx = OTXv2("")
+#otx = OTXv2(str(dotenv_values('OTX_API_KEY')))
+otx = OTXv2("1d676bb0a0c8f2e7be40b7636a86b5c9f977b668a787b8656662d2dd5eb37767")
 
 
 pulses=otx.search_pulses("Brazil")
-print(pandas.json_normalize(pulses["results"]))
+#print(pandas.json_normalize(pulses["results"]))
 pdtable=pandas.DataFrame()
 for i in range(len(pulses["results"])):
         pulse_id=pulses["results"][i]["id"]
         pulse_details = otx.get_pulse_details(pulse_id)
-        #pdtable.append(otx.get_pulse_details(pulse_id))
-        print(pandas.json_normalize(pulse_details))
+        #print(pandas.json_normalize(pulse_details))
 
-
+        pdtable=pandas.json_normalize(pulse_details)
+        #print(pdtable.head(10))
+pdtable.to_json('./otxbr.json')
