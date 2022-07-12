113e840fed8377fa34743dd64ff143d416aede4b
mth <matheusoliveiratux4me@gmail.com>
2022-07-12 12:42:37 -0300

    :smile: add two more querys to get more results and more data to compare

--- otx.py
+++ otx.py
@@ -12,20 +12,50 @@ load_dotenv()
 #otx = OTXv2(str(dotenv_values('OTX_API_KEY')))
 otx = OTXv2("1d676bb0a0c8f2e7be40b7636a86b5c9f977b668a787b8656662d2dd5eb37767")
 
-
+#CONSULTAS PARA PULSES RELACIONADOS AO BRASIL
 pulses=otx.search_pulses("Brazil")
-#print(pandas.json_normalize(pulses["results"]))
+p2=otx.search_pulses("BR")
+p3=otx.search_pulses("Brasil")
+#criação de dataframes
 pdtable=pandas.DataFrame()
+pd2 = pd.DataFrame()
+pd3 = pd.DataFrame()
+#criação de vetores temporários que auxiliam no manuseio dos dados
 dta=[]
+dta2=[]
+dta3=[]
+#ler resultados das querys e puchar os detalhes para obter mais dados das ocorrencias
 for i in range(len(pulses["results"])):
         pulse_id=pulses["results"][i]["id"]
         pulse_details = otx.get_pulse_details(pulse_id) 
         dta.append(pulse_details)
 
-for i in range(len(dta)):
+for i in range(len(dta)): #montagem dataframe pdtable
         pdtable = pdtable.append(dta[i] , ignore_index=True)
+for i in range(len(p2['results'])):
+        p2_id=p2["results"][i]["id"]
+        p2_details = otx.get_pulse_details(p2_id)
+        dta2.append(p2_details)
 
-pdtable.to_csv('./otxbr.csv',index=False)      
+for i in range(len(p3['results'])):
+        p3_id=p3["results"][i]["id"]
+        p3_details = otx.get_pulse_details(p3_id)
+        dta3.append(p3_details)
+
+#manuseio de resultados para montagem de dataframes pd2 e pd3
+for i in range(len(dta2)):
+  pd2 = pd2.append(dta2[i] , ignore_index=True)
+
+for i in range(len(dta3)):
+  pd3 = pd3.append(dta3[i] , ignore_index=True)
+
+#montagem de dataframe final com os três dataframes concatenados
+f=[pdtable,pd2,pd3] #variavel que armazena o conteúdo dos três dataframes
+finalbr=pd.concat(f,ignore_index=True) #concatenação dos três dataframes
+
+finalbr.to_csv('./otbrfinal.csv', index=False) #exportando o dataframe em um arquivo csv
+
+#consulta personalizada por dominio
 def consulta_dominio(otx):
     data=[]
     dominio=str(input('digite o dominio que deseja buscar'))
